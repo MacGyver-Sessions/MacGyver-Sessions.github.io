ac8819ea3e3803b5815262a472e2ea5cd45d52a9
Kristina Collins <kvc2@case.edu>
2025-12-08 10:58:33 -0500

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'agu', 'publications', 'awards']
+const section_names = ['home', 'agu', 'publications', 'awards', 'howto']
 
 
 window.addEventListener('DOMContentLoaded', event => {
